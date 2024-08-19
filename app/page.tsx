52789a168ad02aa5c88aa54ef68066d90718c790
Abdus Sattar <sasorganicagro@gmail.com>
2024-08-19 14:29:05 +0600

    Install Nextjs for frontend of the Zoom_clone app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-purple-500 first-letter:text-stone-800 text-justify font-bold text-3xl">
+        Hello Zoom clone web app
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
